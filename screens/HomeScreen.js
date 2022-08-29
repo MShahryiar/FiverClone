5c0469ca1085f97aa6b55d61b23b20b36764486a
MShahryiar <muhammadshahryiar@yahoo.com>
2022-08-29 19:47:13 +0500

    Fiver Clone Initial Commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text className="text-red-600">HomeScreen</Text>
+      <Text className="text-green-500">Fiver Clone</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
