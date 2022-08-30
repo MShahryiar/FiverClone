48f2e6eed26da03b61ee23ed9153279e2b7b173e
MShahryiar <muhammadshahryiar@yahoo.com>
2022-08-30 10:55:10 +0500

    Text Color change

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,22 @@
 import { View, Text,StatusBar, Image } from 'react-native'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import avatar from "../assets/images/avatar.jpg";
 import * as Progress from 'react-native-progress';
 
 const HomeScreen = () => {
-  const [initialRating, setInitialRating] = useState(1);
-  const [orderCompletion, setOrderCompletion] = useState(0.7);  
-  const [responseRate, setResponseRate] = useState(1);
-  const [onTimeDelivery, setOnTimeDelivery] = useState(1);
+  const [initialRating, setInitialRating] = useState(0);
+  const [orderCompletion, setOrderCompletion] = useState(0);  
+  const [responseRate, setResponseRate] = useState(0);
+  const [onTimeDelivery, setOnTimeDelivery] = useState(0);
+
+ useEffect(()=>{
+    setTimeout(()=>{
+     setInitialRating(0.9)
+     setOrderCompletion(0.7)
+     setResponseRate(0.96)
+     setOnTimeDelivery(1)
+    },2000)
+  },[])
 
   return (
     <>
@@ -60,22 +69,22 @@ const HomeScreen = () => {
             <View className="flex-row justify-between px-4 my-5">
                 <View className="flex-row space-x-5">
                 <View>
-                <Progress.Circle size={80} color="lightgreen" progress={responseRate} showsText={true} formatText={()=>responseRate*100 + " %"}/>
+                <Progress.Circle size={80} color="lightgreen" progress={responseRate} textStyle={{color:'white', fontWeight:'bold', fontSize:20}} showsText={true} formatText={()=>responseRate*100 + " %"}/>
                 <Text className="text-center text-white text-md font-bold mt-3">Response</Text>
                 <Text className="text-center  font-bold text-white text-md">Rate</Text>
                 </View>
                 <View>
-                <Progress.Circle size={80} color="lightgreen" progress={orderCompletion} showsText={true} formatText={()=>orderCompletion*100 + " %"}/>
+                <Progress.Circle size={80} color="lightgreen" progress={orderCompletion} unfilledColor="grey" borderColor='grey' textStyle={{color:'white', fontWeight:'bold', fontSize:20}} showsText={true} formatText={()=>orderCompletion*100 + " %"}/>
                 <Text className="text-center text-white text-md font-bold mt-3">Order</Text>
                 <Text className="text-center  font-bold text-white text-md">Completion</Text>
                 </View>
                 <View>
-                <Progress.Circle size={80} color="lightgreen" progress={onTimeDelivery} showsText={true} formatText={()=>onTimeDelivery*100+ " %"}/>
+                <Progress.Circle size={80} color="lightgreen" progress={onTimeDelivery} textStyle={{color:'white', fontWeight:'bold', fontSize:20}} showsText={true} formatText={()=>onTimeDelivery*100+ " %"}/>
                 <Text className="text-center text-white text-md font-bold mt-3">On-Time</Text>
                 <Text className="text-center  font-bold text-white text-md">Delivery</Text>
                 </View>
                 <View>
-                <Progress.Circle size={80} color="lightgreen"  progress={initialRating} showsText={true} formatText={()=>initialRating/2*10}/>
+                <Progress.Circle size={80} color="lightgreen" textStyle={{color:'white', fontWeight:'bold', fontSize:20}} progress={initialRating} showsText={true} formatText={()=>initialRating/2*10}/>
                 <Text className="text-center text-white text-md font-bold mt-3">Positive</Text>
                 <Text className="text-center  font-bold text-white text-md">Rating</Text>
                 </View>
