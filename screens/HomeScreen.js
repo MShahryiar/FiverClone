0966398dafab3ded02c8186290dbba7ef810cd94
MShahryiar <muhammadshahryiar@yahoo.com>
2022-09-01 10:40:16 +0500

    Fixed Layout Issue

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,7 +2,7 @@ import { View, Text,StatusBar, Image, ScrollView } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import avatar from "../assets/images/avatar.jpg";
 import * as Progress from 'react-native-progress';
-import { ArrowUpIcon } from "react-native-heroicons/outline"
+import { ArrowUpIcon, ChevronDownIcon, QuestionMarkCircleIcon } from "react-native-heroicons/outline"
 
 const HomeScreen = () => {
   const [initialRating, setInitialRating] = useState(0);
@@ -49,11 +49,11 @@ const HomeScreen = () => {
                   Standards to Maintain
                 </Text>
                 <Text className="text-white">
-                  ?
+                  <QuestionMarkCircleIcon color="white"/>
                 </Text>
             </View>
             <View className="flex-row justify-between px-4 my-5">
-                <Text className="text-white font-semibold">
+                <Text className="text-white">
                   Seller Level
                 </Text>
                 <Text className="text-white">
@@ -61,15 +61,15 @@ const HomeScreen = () => {
                 </Text>
             </View>
             <View className="flex-row justify-between px-4 my-5">
-                <Text className="text-white font-semibold">
+                <Text className="text-white">
                   Next Evaluation
                 </Text>
-                <Text className="font-bold" style={{color:"#1dbf73"}}>
+                <Text className="text-gray-400">
                   Sep, 15, 2022
                 </Text>
             </View>
             <View className="flex-row justify-between px-4 my-5">
-                <Text className="text-white font-semibold">
+                <Text className="text-white">
                   Response Time
                 </Text>
                 <Text className="font-bold" style={{color:"#1dbf73"}}>
@@ -79,7 +79,7 @@ const HomeScreen = () => {
             <View className="flex-row justify-between px-4 my-5">
                 <View className="flex-row space-x-5">
                 <View>
-                <Progress.Circle size={80} color="#1dbf73" progress={responseRate} textStyle={{color:'white', fontWeight:'bold', fontSize:20}} showsText={true} formatText={()=>responseRate*100 + " %"}/>
+                <Progress.Circle size={80} color="#1dbf73" progress={responseRate} unfilledColor="grey" textStyle={{color:'white', fontWeight:'bold', fontSize:20}} showsText={true} formatText={()=>responseRate*100 + " %"}/>
                 <Text className="text-center text-white text-md font-bold mt-3">Response</Text>
                 <Text className="text-center  font-bold text-white text-md">Rate</Text>
                 </View>
@@ -89,12 +89,12 @@ const HomeScreen = () => {
                 <Text className="text-center  font-bold text-white text-md">Completion</Text>
                 </View>
                 <View>
-                <Progress.Circle size={80} color="#1dbf73" progress={onTimeDelivery} textStyle={{color:'white', fontWeight:'bold', fontSize:20}} showsText={true} formatText={()=>onTimeDelivery*100+ " %"}/>
+                <Progress.Circle size={80} color="#1dbf73" progress={onTimeDelivery} unfilledColor="grey" textStyle={{color:'white', fontWeight:'bold', fontSize:20}} showsText={true} formatText={()=>onTimeDelivery*100+ " %"}/>
                 <Text className="text-center text-white text-md font-bold mt-3">On-Time</Text>
                 <Text className="text-center  font-bold text-white text-md">Delivery</Text>
                 </View>
                 <View>
-                <Progress.Circle size={80} color="#1dbf73" textStyle={{color:'white', fontWeight:'bold', fontSize:20}} progress={initialRating} showsText={true} formatText={()=>initialRating/2*10}/>
+                <Progress.Circle size={80} color="#1dbf73" progress={initialRating} unfilledColor="grey" textStyle={{color:'white', fontWeight:'bold', fontSize:20}}  showsText={true} formatText={()=>initialRating/2*10}/>
                 <Text className="text-center text-white text-md font-bold mt-3">Positive</Text>
                 <Text className="text-center  font-bold text-white text-md">Rating</Text>
                 </View>
@@ -102,7 +102,7 @@ const HomeScreen = () => {
             </View>
             <View className="flex-row justify-between">
               <Text className="text-lg font-bold text-white my-3 mx-2">Reach your next level</Text>
-              <Text className="text-lg font-bold text-white my-3 mx-2" onPress={()=>setShowDetails(!details)}>Drop Down</Text>
+              <Text className="text-lg font-bold text-white my-3 mx-2" onPress={()=>setShowDetails(!details)}><ChevronDownIcon color="white" /></Text>
               
             </View>
             {details && (
@@ -153,33 +153,33 @@ const HomeScreen = () => {
                 <Text className="text-[#1dbf73]">Details</Text>
         </View>
         <View className="w-100 mx-3 bg-white rounded-md ">
-          <View className='flex-row'>          
-                <View className="m-2">
-                  <Text className='text-sm'>Personal Balance</Text>
+          <View className='flex-row p-1 items-center'>          
+                <View className="m-2 w-40">
+                  <Text className=''>Personal Balance</Text>
                   <Text className="font-bold text-lg text-[#1dbf73]">$650</Text>
                 </View>
-                <View className="m-2 ml-20 ">
-                  <Text className='text-sm'>Earning in August</Text>
+                <View className="">
+                  <Text className=''>Earning in August</Text>
                   <Text className="font-bold text-lg text-[#1dbf73]">$400</Text>
                 </View>
           </View>
-          <View className='flex-row'>          
-                <View className="m-2">
-                  <Text className='text-sm'>Avg. Selling Price</Text>
+          <View className='flex-row p-1 items-center'>          
+                <View className="m-2 w-40">
+                  <Text className=''>Avg. Selling Price</Text>
                   <Text className="font-bold text-lg text-[#1dbf73]">$55</Text>
                 </View>
-                <View className="m-2 ml-20 ">
-                  <Text className='text-sm'>Active Orders</Text>
+                <View className="">
+                  <Text className=''>Active Orders</Text>
                   <Text className="font-bold text-lg text-[#1dbf73]">2 ($75)</Text>
                 </View>
           </View>
-          <View className='flex-row'>          
-                <View className="m-2">
-                  <Text className='text-sm'>Pending Clearance</Text>
+          <View className='flex-row p-1 items-center' >          
+                <View className="m-2  w-40">
+                  <Text className=''>Pending Clearance</Text>
                   <Text className="font-bold text-lg text-[#1dbf73]">$0</Text>
                 </View>
-                <View className="m-2 ml-20 ">
-                  <Text className='text-sm'>Cancelled Orders</Text>
+                <View className="">
+                  <Text className=''>Cancelled Orders</Text>
                   <Text className="font-bold text-lg text-[#1dbf73]">1 ($45)</Text>
                 </View>
           </View>
@@ -210,11 +210,11 @@ const HomeScreen = () => {
           </View>
           <View className='flex-row mt-3 items-center justify-between'>  
                   <Text className='text-sm'>Impressions</Text>
-                  <Text className="text-xs text-gray-600">1.6k <ArrowUpIcon color={'#1dbf73'} className="font-bold" size={15}/></Text>
+                  <Text className="text-xs text-gray-600 font-bold">1.6k <ArrowUpIcon color={'#1dbf73'} className="font-bold" size={15}/></Text>
           </View>
           <View className='flex-row mt-3 items-center justify-between'>  
                   <Text className='text-sm'>Clicks</Text>
-                  <Text className="text-xs text-gray-600">500 <ArrowUpIcon color={'#1dbf73'} className="font-bold" size={15}/></Text>
+                  <Text className="text-xs text-gray-600 font-bold">500 <ArrowUpIcon color={'#1dbf73'} size={15}/></Text>
 
           </View>
         </View>
